34c633c82bffae55bf72561188d60129f961bdb3
Benjamin Reese <bareese@slac.stanford.edu>
2020-05-12 08:43:20 -0700

    Use library

--- protocols/pgp/pgp3/asic/rtl/Pgp3TxLiteWrapper.vhd
+++ protocols/pgp/pgp3/asic/rtl/Pgp3TxLiteWrapper.vhd
@@ -29,17 +29,16 @@ entity Pgp3TxLiteWrapper is
    generic (
       TPD_G : time := 1 ns);
    port (
-      clk         : in  sl;
-      rst         : in  sl;
-      txReady     : out sl;
-      txValid     : in  sl;
-      txData      : in  slv(63 downto 0);
-      txSof       : in  sl;
-      txEof       : in  sl;
-      txEofe      : in  sl;
-      phyTxValid  : out sl;
-      phyTxData   : out slv(63 downto 0);
-      phyTxHeader : out slv(1 downto 0));
+      clk        : in  sl;
+      rst        : in  sl;
+      txReady    : out sl;
+      txValid    : in  sl;
+      txData     : in  slv(63 downto 0);
+      txSof      : in  sl;
+      txEof      : in  sl;
+      txEofe     : in  sl;
+      phyTxValid : out sl;
+      phyTxData  : out slv(65 downto 0));
 
 end entity Pgp3TxLiteWrapper;
 
@@ -60,8 +59,7 @@ architecture rtl of Pgp3TxLiteWrapper is
 
 begin
 
-   txReady <= pgpTxSlave.tReady;
-
+   txReady                        <= pgpTxSlave.tReady;
    pgpTxMaster.tValid             <= txValid;
    pgpTxMaster.tData(63 downto 0) <= txData;
    pgpTxMaster.tKeep(7 downto 0)  <= X"FF";
@@ -69,26 +67,27 @@ begin
    pgpTxMaster.tUser(14)          <= txEofe;
    pgpTxMaster.tLast              <= txEof;
 
-   U_Pgp3TxLite_1 : entity work.Pgp3TxLite
+
+   U_Pgp3TxLite_1 : entity surf.Pgp3TxLite
       generic map (
          TPD_G    => TPD_G,
          NUM_VC_G => 1)
       port map (
-         pgpTxClk        => clk,           -- [in]
-         pgpTxRst        => rst,           -- [in]
-         pgpTxIn         => pgpTxIn,       -- [in]
-         pgpTxOut        => open,          -- [out]
-         pgpTxMasters(0) => pgpTxMaster,   -- [in]
-         pgpTxSlaves(0)  => pgpTxSlave,    -- [out]
+         pgpTxClk        => clk,                       -- [in]
+         pgpTxRst        => rst,                       -- [in]
+         pgpTxIn         => pgpTxIn,                   -- [in]
+         pgpTxOut        => open,                      -- [out]
+         pgpTxMasters(0) => pgpTxMaster,               -- [in]
+         pgpTxSlaves(0)  => pgpTxSlave,                -- [out]
 --          locRxFifoCtrl  => locRxFifoCtrl,   -- [in]
 --          locRxLinkReady => locRxLinkReady,  -- [in]
 --          remRxFifoCtrl  => remRxFifoCtrl,   -- [in]
 --          remRxLinkReady => remRxLinkReady,  -- [in]
-         phyTxActive     => '1',           -- [in]
-         phyTxReady      => '1',           -- [in]
-         phyTxValid      => phyTxValid,    -- [out]
-         phyTxStart      => open,          -- [out]
-         phyTxData       => phyTxData,     -- [out]
-         phyTxHeader     => phyTxHeader);  -- [out]
+         phyTxActive     => '1',                       -- [in]
+         phyTxReady      => '1',                       -- [in]
+         phyTxValid      => phyTxValid,                -- [out]
+         phyTxStart      => open,                      -- [out]
+         phyTxData       => phyTxData(63 downto 0),    -- [out]
+         phyTxHeader     => phyTxData(65 downto 64));  -- [out]
 
 end architecture rtl;
